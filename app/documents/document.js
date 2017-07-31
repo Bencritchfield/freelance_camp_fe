fbc2c353c7544400a6176fb9f017b78c59e95cf8
Ben Critchfield <ben.critchfield8@gmail.com>
2017-07-31 14:52:58 -0600

    added doc interface

---
+++ app/documents/document.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+image - url;
+string,
+;
+//# sourceMappingURL=document.js.map
---
+++ app/documents/document.ts
@@ -0,0 +1,7 @@
+export interface document {
+	title: string,
+	description: string, 
+	file_url: string, 
+	update_at: string, 
+	image-url: string, 
+}
