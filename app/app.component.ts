48d51565f2c5985ad6c207f511e1908d3f02990f
Ben Critchfield <ben.critchfield8@gmail.com>
2017-07-31 14:08:10 -0600

    added homepage tittle and others to app

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,11 +3,9 @@ import { Component } from '@angular/core';
 @Component({
 	moduleId: module.id,
 	selector: 'app',
-	template: `
-	<h1>Navigation</h1>
+	templateUrl: 'app.component.html'
 
-	<router-outlet></router-outlet>
-	`
+	
 })
 
 export class AppComponent {
