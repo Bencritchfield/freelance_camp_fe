29c418b5b6a4b982e119e09cc0d67e072bc7898c
Ben Critchfield <ben.critchfield8@gmail.com>
2017-08-01 10:22:48 -0600

    created text box for proposal

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -9,11 +10,11 @@ import { ProposalNewComponent  } from './proposal/proposal-new.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
 	imports: [
 		BrowserModule,
-		AppRoutingModule
+		AppRoutingModule,
+		FormsModule
 	],
 	declarations: [
 		AppComponent,
