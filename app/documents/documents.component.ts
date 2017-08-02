99e951ed8e21b13226c80fc603302af3c45250b0
Ben Critchfield <ben.critchfield8@gmail.com>
2017-08-02 09:25:08 -0600

    added pics to docs

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -15,21 +15,21 @@ export class DocumentsComponent {
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'http://www.surelyjobs.com/wp-content/uploads/2016/02/edfsdfs.jpg',
 		},
 		{
 			title: "My Second Doc",
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'http://www.surelyjobs.com/wp-content/uploads/2016/02/edfsdfs.jpg',
 		},
 		{
 			title: "My Last Doc",
 			description: 'asdfasdfasdf asdfasd',
 			file_url: 'http://google.com',
 			updated_at: '11/11/16',
-			image_url: 'http://google.com',
+			image_url: 'http://www.surelyjobs.com/wp-content/uploads/2016/02/edfsdfs.jpgd',
 		}
 	]
 }
